aa9994eeb39b9a0a84deede4a162eb4a13ceabf6
akim157 <akim157@mail.ru>
2019-09-19 19:03:49 +0300

    v.1.0.3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hi');
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        };
+    }
+    inputOnChange(event) {
+        const text = event.target.value;
+        this.setState({ text });
+    }
+    render() {
+        return (
+            <div className='test'>
+                <h1>Hello</h1>
+                <input type="text" value={ this.state.text } onChange={ this.inputOnChange.bind(this) }/>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('app')
+);
