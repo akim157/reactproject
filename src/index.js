c3b832aaf784882e5c31e005ec214e42715b16da
akim157 <akim157@mail.ru>
2019-09-26 18:27:29 +0300

    v.1.0.4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router-dom';
+import { Provider } from 'react-redux'; //провайдер связывает Store со всеми наши элементами
+import routes from './routes';
+import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render((
+    <Provider store={ store }>
+        <Router history={ history }>
+            { routes }
+        </Router>
+    </Provider>
+), document.querySelector('#app'));
